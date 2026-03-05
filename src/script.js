31a8eb4e22d0587d920d35127854241e22769e2e
giftin.id <calistanafisaa@gmail.com>
2026-03-05 22:29:59 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@
 
         function checkPassword() {
             const password = document.getElementById('passwordInput').value;
-            if (password.toLowerCase() === 'sritex') {
+            if (password.toLowerCase() === '060126') {
                 document.getElementById('loginScreen').style.display = 'none';
                 document.getElementById('mainContent').style.display = 'block';
             } else {
